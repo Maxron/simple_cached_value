8713a54ee3f4a063e128b7d64f8d43e9368dc26a
Wei <zero.maxima@gmail.com>
2025-07-20 20:48:22 +0800

    feat: remove unuse files

--- lib/simple_cached_value.dart
+++
@@ -1,8 +0,0 @@
-
-import 'simple_cached_value_platform_interface.dart';
-
-class SimpleCachedValue {
-  Future<String?> getPlatformVersion() {
-    return SimpleCachedValuePlatform.instance.getPlatformVersion();
-  }
-}
